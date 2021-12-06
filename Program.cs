d56666daec6491889e848b61d0c7a4b7ab00caf0
Brendon Ignath <brendon.ignath@gmail.com>
2021-12-06 20:05:56 +0100

    Day 2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace AdventOfCode {
     public class Program {
-        public static void Main() => Console.WriteLine(Day1(File.ReadAllLines("input/day1.txt")));
+        public static void Main() => Console.WriteLine(Day2(File.ReadAllLines("input/day2.txt")));
 
         public static int Day1(string[] rawInput) {
 			int[] input = rawInput.Select(int.Parse).ToArray();
@@ -25,5 +25,26 @@ namespace AdventOfCode {
             }
 			return c;
         }
+
+		public static int Day2(string[] rawInput) {
+			int h = 0, d = 0, a = 0;
+
+			// Part 1: 1727835
+			/* foreach (var line in rawInput) {
+				var cmd = line.Split(' ');
+				h += cmd[0] == "forward" ? int.Parse(cmd[1]) : 0;
+				d += cmd[0] == "up" ? int.Parse(cmd[1])*-1 : cmd[0] == "down" ? int.Parse(cmd[1]) : 0;
+			}*/
+
+			// Part 2: 1544000595
+			foreach (var line in rawInput) {
+				var cmd = line.Split(' ');
+				int f = cmd[0] == "forward" ? int.Parse(cmd[1]) : 0;
+				h += f;
+				d += f*a;
+				a += cmd[0] == "up" ? int.Parse(cmd[1])*-1 : cmd[0] == "down" ? int.Parse(cmd[1]) : 0;
+			}
+			return h*d;
+		}
     }
 }
